53cb4fcd97a4c3036890e4a68960e2b550c39230
Piotr Bilski <piotrbilski@gmail.com>
2014-03-16 19:10:52 +0100

    - Add thread to handle picture grabbing

--- UI/MainWindow.py
+++ UI/MainWindow.py
@@ -6,21 +6,39 @@ Created on 15-03-2014
 import sys
 import numpy
 import cv2
+import time
 from PySide import QtCore, QtGui
 
 
-class QCamera(QtCore.QObject):
+class QCamera(QtCore.QThread):
     
     PictureSignal = QtCore.Signal(numpy.ndarray)
+    CameraStateSignal = QtCore.Signal(bool)
+    
     cap = None
     
     def __init__(self, *args, **kwargs):
-        QtCore.QObject.__init__(self, *args, **kwargs)
-        self.cap = cv2.VideoCapture(0)
+        QtCore.QThread.__init__(self, *args, **kwargs)
+        self.sayCheese = True
+        try:
+            self.cap = cv2.VideoCapture(0)
+            self.CameraStateSignal.emit(True)
+        except Exception:
+            self.CameraStateSignal.emit(False)
+            pass
 
+    def run(self):
+        while self.sayCheese:
+            self.capturePic()
+        self.sayCheese = True
+
+            
     def capturePic(self):
         ret, frame = self.cap.read()
         self.PictureSignal.emit(frame)
+        
+    def stopThread(self):
+        self.sayCheese = False
         
         
 
@@ -35,7 +53,9 @@ class MainWindow(QtGui.QWidget):
         self.setWindowTitle("Webcam Tools")
         
         self.camera = QCamera()
+        
         self.camera.PictureSignal.connect(self.displayPic)
+        self.camera.CameraStateSignal.connect(self.readyCamera)
         
         hLayout = QtGui.QHBoxLayout()
         self.leftLabel = QtGui.QLabel('Left pic')
@@ -47,9 +67,9 @@ class MainWindow(QtGui.QWidget):
         
         vLayout = QtGui.QVBoxLayout()
         vLayout.addLayout(hLayout)
-        makePic = QtGui.QPushButton("Make a pic")
-        makePic.clicked.connect(self.capturePic)
-        vLayout.addWidget(makePic)
+        self.makePic = QtGui.QPushButton("Start")
+        self.makePic.clicked.connect(self.capturePic)
+        vLayout.addWidget(self.makePic)
         
         
         self.setLayout(vLayout)
@@ -61,6 +81,12 @@ class MainWindow(QtGui.QWidget):
         qimg = QtGui.QImage(picture.data,picture.shape[1], picture.shape[0], QtGui.QImage.Format_RGB888)
         return QtGui.QPixmap.fromImage(qimg)
 
+    def readyCamera(self, status):
+        if status:
+            self.makePic.setEnabled()
+            return
+        self.makePic.setDisabled()
+            
         
     def displayPic(self, picArray):
         
@@ -68,7 +94,11 @@ class MainWindow(QtGui.QWidget):
         self.rightLabel.setPixmap(self.__arrayToQPixmap(picArray))
         
     def capturePic(self):
-        self.camera.capturePic()
+        if self.camera.isRunning():
+            self.camera.stopThread()
+        else:
+            self.camera.start()
+        
 
 if __name__ == '__main__':
     
